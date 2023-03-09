687a3018743b71ffd3d6e17033b46f9c07ebdc99
Simone Falcini <simone@falcini.com>
2023-03-09 10:34:53 +0100

    remove unfurl

--- src/simonefalcini/SlackTarget/SlackTarget.php
+++ src/simonefalcini/SlackTarget/SlackTarget.php
@@ -211,8 +211,8 @@ class SlackTarget extends \yii\log\Target
 
 			$blocks[] =	[
 				'type' => 'section',
-				'unfurl_links' => false,
-				'unfurl_media' => false,
+				//'unfurl_links' => false,
+				//'unfurl_media' => false,
 				'text' => [
 					'type' => 'mrkdwn',
 					'text' => "*File:* " . $error_file . "\n*Row:* " . $error_line . "\n`> " . $current_url . "`",
